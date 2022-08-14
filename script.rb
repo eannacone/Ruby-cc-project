91059feeb5de2f20f9652dc55947da4377627a29
Eric <eric.annacone@gmail.com>
2022-08-14 13:44:02 -0600

    Add array with shifted numbers

--- script.rb
+++ script.rb
@@ -1,7 +1,7 @@
 def caesar_cipher (convert, shift=0)
     broken_down = []
     numerical_conv = []
-  
+    num_shift = []
     # break down string by letter
     for i in convert.split('') do
       broken_down.push(i)
@@ -11,12 +11,21 @@ def caesar_cipher (convert, shift=0)
     broken_down.each do |a|
       numerical_conv.push(a.ord)
     end
+
+    # shift numerals to new values
+    numerical_conv.each do |b|
+      num_shift.push(b + shift)
+    end
+
+    
+  
     puts "Your word to convert is '#{convert}'"
     puts "Broken down word by letter is #{broken_down}"
     puts "Letters to numeric is #{numerical_conv}" 
+    puts "New numerical values are #{num_shift}"
 end
 
-caesar_cipher('string', 0)
+caesar_cipher('string', 5)
 
 
 
