42eb561eddc5936ba6bb80078be656fae7ce42be
Eric <eric.annacone@gmail.com>
2022-08-14 13:48:03 -0600

    Add translator back to character form

--- script.rb
+++ script.rb
@@ -2,6 +2,7 @@ def caesar_cipher (convert, shift=0)
     broken_down = []
     numerical_conv = []
     num_shift = []
+    final = []
     # break down string by letter
     for i in convert.split('') do
       broken_down.push(i)
@@ -17,15 +18,19 @@ def caesar_cipher (convert, shift=0)
       num_shift.push(b + shift)
     end
 
-    
+    # translate new numerals to letters
+    num_shift.each do |c|
+      final.push(c.chr)
+    end
   
     puts "Your word to convert is '#{convert}'"
     puts "Broken down word by letter is #{broken_down}"
     puts "Letters to numeric is #{numerical_conv}" 
     puts "New numerical values are #{num_shift}"
+    puts "Final product with shifted characters is #{final}"
 end
 
-caesar_cipher('string', 5)
+caesar_cipher('abcdef', 1)
 
 
 
