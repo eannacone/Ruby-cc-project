0465d236a2addfbc96ffce8740a8bf914bd6fc7b
Eric <eric.annacone@gmail.com>
2022-09-23 13:41:35 -0600

    Begin tic tac toe game, gameboard made

--- bubsort.rb
+++ bubsort.rb
@@ -1,9 +1,18 @@
 def bubble_sort (list)
     # basecase, return list if it has one element or less
     return list if list.size <= 1
-
-
-
+    swapped = true
+    # while swapped is true keep swapping integers 
+    while swapped do
+        swapped = false
+        0.upto(list.size-2) do |i|
+            if list[i] > list[i+1]
+                list[i], list[i+1] = list[i+1], list[i]
+                swapped = true
+            end
+        end
+    end
+    puts list
 end
 
 
---
+++ tictac.rb
@@ -0,0 +1,10 @@
+class Board
+    def initialize()
+      puts "---
+  ---
+  ---"
+    end
+  end
+  
+  new_game = Board.new
+  puts new_game
