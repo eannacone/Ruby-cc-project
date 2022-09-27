a8befd0c22e4d1f200e3a9b28a8f374ca3c1d511
Eric <eric.annacone@gmail.com>
2022-09-26 18:48:45 -0600

    Add board and player info.

--- tictac.rb
+++ tictac.rb
@@ -1,10 +1,19 @@
-class Board
-    def initialize()
-      puts "---
-  ---
-  ---"
-    end
-  end
+class Game
+    @@turn_count = 1
+    @@winner = ''
   
-  new_game = Board.new
-  puts new_game
+    def initialize()
+      puts 'Player 1 - enter your name!'
+      @player_one_name = gets.chomp
+      puts 'Player 2 - enter your name!'
+      @player_two_name = gets.chomp
+      @board = Array.new(3) {Array.new(3, '_')}
+    end
+  
+    def display_board(board)
+      puts '\r'
+      puts '#{board[0][0]} | #{board[0][1]} | #{board[0][2]}'
+      puts '#{board[1][0]} | #{board[1][1]} | #{board[1][2]}'
+      puts '#{board[2][0]} | #{board[2][1]} | #{board[2][2]}'
+      puts '\r'
+    end
